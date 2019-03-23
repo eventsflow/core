106ebdc39ca160e10fe025a6bdc15df17317651e
ownport <ownport@gmail.com>
2019-03-23 17:15:06 +0200

    [+] more tests

--- test/queues/local/test_events_queue.py
+++ test/queues/local/test_events_queue.py
@@ -25,3 +25,43 @@ def test_events_queue_publish_consume_commit():
     assert received_event.metadata == event.metadata
     assert received_event.payload == event.payload
 
+def test_events_queue_with_size():
+
+    QUEUE_SIZE = 5
+
+    events_queue = EventsQueue(size=QUEUE_SIZE)
+    event = Event(name='TestEvent', metadata={'k1': 'v1'}, payload=['TestEventPayload', ])
+
+    for _ in range(QUEUE_SIZE):
+        events_queue.publish(event)
+    
+    events = []
+    for _ in range(QUEUE_SIZE):
+        events.append(events_queue.consume())
+    
+    assert len(events) == QUEUE_SIZE
+
+def test_events_queue_overflow():
+
+    from eventsflow.queues.local.queues import LocalQueueFull
+
+    QUEUE_SIZE = 6
+
+    events_queue = EventsQueue(size=QUEUE_SIZE-1)
+    event = Event(name='TestEvent', metadata={'k1': 'v1'}, payload=['TestEventPayload', ])
+
+    with pytest.raises(LocalQueueFull):
+        for _ in range(QUEUE_SIZE):
+            events_queue.publish(event, timeout=1)
+    
+def test_events_queue_many_commits():
+
+    events_queue = EventsQueue()
+    
+    event = Event(name='TestEvent', metadata={'k1': 'v1'}, payload=['TestEventPayload', ])
+    events_queue.publish(event)
+
+    with pytest.raises(ValueError):
+        event = events_queue.consume()
+        events_queue.commit()
+        events_queue.commit()
