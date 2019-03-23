90962532d98ec8ac28bf0c98bf654b87123ea0c2
ownport <ownport@gmail.com>
2019-03-23 17:14:39 +0200

    [*] renamed unfinished tasks to events

--- eventsflow/queues/local/queues.py
+++ eventsflow/queues/local/queues.py
@@ -5,6 +5,10 @@ __all__ = [ 'EventsQueue', ]
 import logging
 
 from collections import Counter
+
+from queue import Empty as LocalQueueEmpty
+from queue import Full as LocalQueueFull
+
 from multiprocessing.queues import Queue
 
 
@@ -16,47 +20,47 @@ class EventsQueue(Queue):
     def __init__(self, maxsize=0, *, ctx):
         
         Queue.__init__(self, maxsize, ctx=ctx)
-        self._unfinished_tasks = ctx.Semaphore(0)
+        self._unfinished_events = ctx.Semaphore(0)
         self._cond = ctx.Condition()
 
     def __getstate__(self):
         
-        return Queue.__getstate__(self) + (self._cond, self._unfinished_tasks)
+        return Queue.__getstate__(self) + (self._cond, self._unfinished_events)
 
     def __setstate__(self, state):
         
         Queue.__setstate__(self, state[:-2])
-        self._cond, self._unfinished_tasks = state[-2:]
+        self._cond, self._unfinished_events = state[-2:]
 
-    def consume(self, timeout=0):
+    def consume(self, block=True, timeout=None):
         ''' consume event from queue
         '''
-        return self.get(timeout=timeout)
+        return self.get(block=block, timeout=timeout)
 
     def publish(self, obj, block=True, timeout=None):
         ''' publish event into queue
         '''
         assert not self._closed
         if not self._sem.acquire(block, timeout):
-            raise Full
+            raise LocalQueueFull
 
         with self._notempty, self._cond:
             if self._thread is None:
                 self._start_thread()
             self._buffer.append(obj)
-            self._unfinished_tasks.release()
+            self._unfinished_events.release()
             self._notempty.notify()
 
     def commit(self):
         
         with self._cond:
-            if not self._unfinished_tasks.acquire(False):
-                raise ValueError('task_done() called too many times')
-            if self._unfinished_tasks._semlock._is_zero():
+            if not self._unfinished_events.acquire(False):
+                raise ValueError('commit() called too many times')
+            if self._unfinished_events._semlock._is_zero():
                 self._cond.notify_all()
 
     def join(self):
         
         with self._cond:
-            if not self._unfinished_tasks._semlock._is_zero():
+            if not self._unfinished_events._semlock._is_zero():
                 self._cond.wait()
