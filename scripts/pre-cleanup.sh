609a9d76bea13cd29f0adc6b23bb661914525421
ownport <ownport@gmail.com>
2019-04-01 13:16:18 +0300

    [+] remove pycache before tests

--- scripts/pre-cleanup.sh
+++ scripts/pre-cleanup.sh
@@ -8,5 +8,8 @@ echo "[INFO] Cleaning build directories" && \
 echo "[INFO] Cleaning files: *.pyc" && \
 	find . -name "*.pyc" -delete
 
+echo "[INFO] Cleaning cache files" && \
+	find . -name "__pycache__" -type d # -exec rm -rf {} \;
+
 echo "[INFO] Cleaning files: *.egg-info" && \
 	rm -rf "*.egg-info"
