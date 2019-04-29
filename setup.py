6765bb95f3d1ef81c9a7e495872ed041b78950c4
ownport <ownport@gmail.com>
2019-04-29 13:54:00 +0300

    Bump version: 0.1.1.dev0 → 0.1.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def build_package_names(path):
 
 setup(
     name='eventsflow',
-    version='0.1.1.dev0',
+    version='0.1.1',
     description="Events Flow",
     long_description=readme,
     packages=build_package_names('eventsflow'),
