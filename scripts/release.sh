83b1a52b612422cca6d969e8c2946489af0f368b
ownport <ownport@gmail.com>
2019-04-29 13:51:42 +0300

    [+] pre-cleanup and make builds

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+echo '[INFO] Cleanup'
+scripts/pre-cleanup.sh
+
+echo '[INFO] Release new version'
 bumpversion part --tag
+python setup.py sdist bdist_wheel
 twine upload --username ownport dist/*
 bumpversion --serialize {major}.{minor}.{patch}.dev0 patch
