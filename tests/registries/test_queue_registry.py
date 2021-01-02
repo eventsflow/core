484856c0030fa0ef1204e985dbaf9d748c38a68a
ownport <ownport@gmail.com>
2021-01-02 20:21:10 +0000

    test: fix linter issues

--- tests/registries/test_queue_registry.py
+++ tests/registries/test_queue_registry.py
@@ -1,40 +1,43 @@
-
+''' Tests for Queues Registry
+'''
 import pytest
 
 from eventsflow.registries.queues import QueuesRegistry
+from eventsflow.queues.local.queues import EventsQueue
 
 
 def test_queues_registry_init():
-
+    ''' test for Queue Registry Initialization
+    '''
     registry = QueuesRegistry()
     assert registry is not None
 
-
 def test_queues_registry_load_empty_queues_config():
-
+    ''' test for load empty queues
+    '''
     registry = QueuesRegistry()
     with pytest.raises(TypeError):
         registry.load([])
 
-
 def test_queues_registry_load_incorrect_queues_config():
-
+    ''' test for load incorrect queues config
+    '''
     registry = QueuesRegistry()
     with pytest.raises(TypeError):
         registry.load({'name': 'TestQueue#1', 'type': 'eventsflow.queues.local.EventsQueue', })
 
-
 def test_queues_registry_load_queues_config_wo_queue_type():
-
+    ''' test for loading queues without queue type
+    '''
     registry = QueuesRegistry()
     with pytest.raises(TypeError):
         registry.load([
             {'name': 'TestQueue#1', },
         ])
 
-
 def test_queues_registry_load_queues_config_with_missed_queue_name():
-
+    ''' test for loading queues with missed queue name
+    '''
     registry = QueuesRegistry()
     with pytest.raises(TypeError):
         registry.load([
@@ -42,19 +45,17 @@ def test_queues_registry_load_queues_config_with_missed_queue_name():
         ])
 
 def test_queues_registry_load_queues_config_with_unk_queue_type():
-
+    ''' tests for loading queue with unknown queue type
+    '''
     registry = QueuesRegistry()
     with pytest.raises(TypeError):
         registry.load([
             {'name': 'TestQueue#1', 'type': 'eventsflow.queues.EventsQueue', },
         ])
 
-
 def test_queues_registry_load():
     ''' load queues to registry
     '''
-    from eventsflow.queues.local.queues import EventsQueue
-
     registry = QueuesRegistry()
     registry.load([
         {'name': 'TestQueue#1', 'type': 'eventsflow.queues.local.EventsQueue', },
--- tests/registries/test_workers_registry_flow.py
+++ tests/registries/test_workers_registry_flow.py
@@ -1,4 +1,5 @@
-
+''' Tests for Workers Registry
+'''
 import time
 import pytest
 
@@ -7,7 +8,7 @@ from eventsflow.registries.workers import WorkersRegistry
 
 from eventsflow.events import EventStopProcessing
 
-from test_common import SampleProcessingWorker
+from tests.test_common import SampleProcessingWorker
 
 
 def test_workers_registry_flow_worker_status():
@@ -15,38 +16,54 @@ def test_workers_registry_flow_worker_status():
     '''
     registry = WorkersRegistry(queues=QueuesRegistry())
     registry.load([
-        {'name': 'TestWorker', 'type': 'test_common.SampleProcessingWorker', },
+        {
+            'name': 'TestWorker',
+            'type': 'tests.test_common.SampleProcessingWorker',
+        },
     ])
 
-    assert [ type(w) for w in registry.workers(status='active')]    == [] 
-    assert [ type(w) for w in registry.workers()]                   == [ SampleProcessingWorker, ] 
-    assert [ type(w) for w in registry.workers(status='all')]       == [ SampleProcessingWorker, ] 
-    assert [ type(w) for w in registry.workers(status='inactive')]  == [ SampleProcessingWorker, ] 
+    assert [ type(w) for w in registry.workers(status='active')] == []
+    assert [ type(w) for w in registry.workers()] == [ SampleProcessingWorker, ]
+    assert [ type(w) for w in registry.workers(status='all')] == [ SampleProcessingWorker, ]
+    assert [ type(w) for w in registry.workers(status='inactive')] == [ SampleProcessingWorker, ]
 
     with pytest.raises(TypeError):
-        assert [ type(w) for w in registry.workers(status='unknown')] == [] 
-
+        assert [ type(w) for w in registry.workers(status='unknown')] == []
 
 def test_workers_registry_flow_start_workers_and_check_status():
     ''' run workers and check workers status
     '''
     queues_registry = QueuesRegistry()
     queues_registry.load([
-        { 'name': 'records', 'type': 'eventsflow.queues.local.EventsQueue' }
+        {
+            'name': 'records',
+            'type': 'eventsflow.queues.local.EventsQueue',
+        }
     ])
-    
+
     workers_registry = WorkersRegistry(queues=queues_registry)
     workers_registry.load([
-        {'name': 'TestWorker', 'type': 'test_common.SampleProcessingWorker', 'inputs': 'records'},
+        {
+            'name': 'TestWorker',
+            'type': 'tests.test_common.SampleProcessingWorker',
+            'inputs': 'records'
+        },
     ])
 
-    assert [ type(w) for w in workers_registry.workers(status='active')]    == [ ] 
+    assert [ type(w) for w in workers_registry.workers(status='active')] == [ ]
 
     workers_registry.start()
-    assert [ type(w) for w in workers_registry.workers(status='active')]    == [ SampleProcessingWorker, ] 
+    assert [
+        type(w) for w in workers_registry.workers(status='active')
+    ] == [ SampleProcessingWorker, ]
 
     queues_registry.get('records').publish(EventStopProcessing())
     time.sleep(1)
 
-    assert [ type(w) for w in workers_registry.workers(status='active')]    == [ ] 
-    assert [ type(w) for w in workers_registry.workers(status='inactive')]    == [ SampleProcessingWorker, ] 
+    assert [
+        type(w) for w in workers_registry.workers(status='active')
+    ] == []
+
+    assert [
+        type(w) for w in workers_registry.workers(status='inactive')
+    ] == [ SampleProcessingWorker, ]
