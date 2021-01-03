c7e4a89e8c8d302d0ddeea61b8cc935e09cb44a1
ownport <ownport@gmail.com>
2021-01-03 17:44:03 +0200

    refact: simplify local queues approach

--- eventsflow/queues/local/__init__.py
+++ eventsflow/queues/local/__init__.py
@@ -1,8 +1,20 @@
+''' Initialize Local Queues Module
+'''
+# import sys
 
-from . import context
+from eventsflow.queues.local import context
 
+from eventsflow.queues.local.queues import QueueFull
+from eventsflow.queues.local.queues import QueueEmpty
+from eventsflow.queues.local.queues import EventsQueue
 
-globals().update((name, getattr(context._default_context, name))
-                 for name in context._default_context.__all__)
-__all__ = context._default_context.__all__
+# # from eventsflow.queues.local.context import default_context
 
+# # __all__ = ['EventsQueue', 'QueueFull', 'QueueEmpty']
+
+# # copy stuff from default context
+__all__ = [x for x in dir(context.default_context) if not x.startswith('_')]
+# __all__.extend(
+#     ''
+# )
+globals().update((name, getattr(context.default_context, name)) for name in __all__)
--- eventsflow/queues/local/queues.py
+++ eventsflow/queues/local/queues.py
@@ -1,13 +1,9 @@
+''' Local Queues
+'''
 
-__all__ = [ 'EventsQueue', ]
-
-
+import queue
 import logging
-
-from collections import Counter
-
-from queue import Empty as LocalQueueEmpty
-from queue import Full as LocalQueueFull
+import multiprocessing
 
 from multiprocessing.queues import Queue
 
@@ -15,61 +11,75 @@ from multiprocessing.queues import Queue
 logger = logging.getLogger(__name__)
 
 
-class EventsQueue(Queue):
+class QueueFull(queue.Full):
+    ''' Exception in case of Local Queue is Full
+    '''
 
-    def __init__(self, maxsize=0, *, ctx):
-        
-        Queue.__init__(self, maxsize, ctx=ctx)
-        self._unfinished_events = ctx.Semaphore(0)
+class QueueEmpty(queue.Empty):
+    ''' Exception in case of Local Queue is Empty
+    '''
+
+class EventsQueue(Queue):
+    ''' Events Queue
+    '''
+    def __init__(self, size=0):
+        ctx = multiprocessing.get_context()
+        Queue.__init__(self, size, ctx=ctx)
+        self._unfinished_tasks = ctx.Semaphore(0)
         self._cond = ctx.Condition()
-        self._maxsize = maxsize
 
     def __getstate__(self):
-        
-        return Queue.__getstate__(self) + (self._cond, self._unfinished_events)
+        return Queue.__getstate__(self) + (self._cond, self._unfinished_tasks)
 
     def __setstate__(self, state):
-        
         Queue.__setstate__(self, state[:-2])
-        self._cond, self._unfinished_events = state[-2:]
-
-    def size(self):
-        ''' Return the approximate size of the queue. 
-        
-        Because of multithreading/multiprocessing semantics, this number is not reliable.
-        '''
-        return self.qsize()
+        self._cond, self._unfinished_tasks = state[-2:]
 
     def consume(self, block=True, timeout=None):
         ''' consume the event from queue
         '''
-        return self.get(block=block, timeout=timeout)
+        try:
+            event = self.get(block=block, timeout=timeout)
+            return event
+        except queue.Empty:
+            raise QueueEmpty from None
 
     def publish(self, obj, block=True, timeout=None):
-        ''' publish the event into queue
+        ''' publish event into queue
         '''
-        assert not self._closed
+        if self._closed:
+            raise ValueError(f"Queue {self!r} is closed")
         if not self._sem.acquire(block, timeout):
-            raise LocalQueueFull
+            raise QueueFull
 
         with self._notempty, self._cond:
             if self._thread is None:
                 self._start_thread()
             self._buffer.append(obj)
-            self._unfinished_events.release()
+            self._unfinished_tasks.release()
             self._notempty.notify()
 
     def commit(self):
-        ''' commit the event in the queue
+        ''' commit event in queue
         '''
         with self._cond:
-            if not self._unfinished_events.acquire(False):
+            if not self._unfinished_tasks.acquire(False):
                 raise ValueError('commit() called too many times')
-            if self._unfinished_events._semlock._is_zero():
+            if self._unfinished_tasks._semlock._is_zero():
                 self._cond.notify_all()
 
     def join(self):
-        
+        ''' join queue
+        '''
         with self._cond:
-            if not self._unfinished_events._semlock._is_zero():
+            if not self._unfinished_tasks._semlock._is_zero():
                 self._cond.wait()
+
+    def size(self):
+        ''' Return the approximate size of the queue.
+        Because of multithreading/multiprocessing semantics, this number is not reliable.
+        '''
+        _size = self.qsize()
+        if self.empty() or _size <= 0:
+            return 0
+        return _size
